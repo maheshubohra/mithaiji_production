6a07888992530cffdec5048eee6ad8750d2e4004
rhv1501 <rhv4748@gmail.com>
2025-04-18 15:52:34 +0530

    made mobile friendly navbar

--- mithaiji/src/comnponents/Navbar.jsx
+++ mithaiji/src/comnponents/Navbar.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Navbar = ({ about, product }) => {
+  const [on, setOn] = useState(false);
+  const handleClick = () => {
+    setOn(!on);
+  };
   return (
     <>
       <div className="w-full h-auto lg:w-full bg-gray-800 fixed top-0 left-0 z-[60] text-white py-1 text-sm lg:text-md">
@@ -7,7 +13,7 @@ const Navbar = ({ about, product }) => {
 
       <nav className="bg-[#FFF8F5] h-auto fixed top-10 left-1/2 border-b-4 border-[#801336] drop-shadow-[#801336] drop-shadow-sm transform -translate-x-1/2 z-70 w-auto lg:w-auto rounded-lg transition-transform duration-500 ease-in-out  hover:scale-105">
         <div className="flex justify-center items-center gap-5 h-full px-4 py-1">
-          <div className="hidden lg:block">
+          <div className="block">
             <a href="/">
               <img
                 src="/assets/mithaiji02.png"
@@ -18,8 +24,24 @@ const Navbar = ({ about, product }) => {
               />
             </a>
           </div>
-          <ul className="flex space-x-4 text-[#801336]">
-            <li className="lg:hidden cursor-pointer">Home</li>
+          <button
+            onClick={handleClick}
+            className="text-black font-extrabold text-2xl"
+          >
+            {on ? "✖" : "☰"}
+          </button>
+          {on && (
+            <ul className="fixed top-17 w-full flex flex-col gap-4 px-5 py-2 bg-[#FFEFEA] rounded-lg shadow-2xl text-[#801336] lg:hidden">
+              <li className="hover:text-[#FFA987] cursor-pointer">
+                <a onClick={about}>About</a>
+              </li>
+              <li className="hover:text-[#FFA987] cursor-pointer">
+                <a onClick={product}>Products</a>
+              </li>
+              <li className="hover:text-[#FFA987] cursor-pointer">Contact</li>
+            </ul>
+          )}
+          <ul className="space-x-4 text-[#801336] hidden lg:flex">
             <li className="hover:text-[#FFA987] cursor-pointer">
               <a onClick={about}>About</a>
             </li>
